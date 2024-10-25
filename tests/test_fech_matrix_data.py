e2b00f98353a4a76edbc514c508576c91dc94d02
Asgef <asgefes1@gmail.com>
2024-10-25 18:10:57 +0300

    Fix tests

--- tests/test_fech_matrix_data.py
+++ tests/test_fech_matrix_data.py
@@ -1,84 +1,61 @@
 import pytest
 import aiohttp
 import asyncio
+from pytest_asyncio import fixture
 from unittest.mock import AsyncMock
 from trainee_assignment.spiral_matrix import fetch_data
 from aiohttp import (
     ClientResponseError, ClientConnectorError, RequestInfo, ClientOSError,
+    ClientSession, ClientResponse
 )
 from aiohttp.typedefs import CIMultiDict
 from aiohttp.typedefs import URL
+from aioresponses import aioresponses
 
 
 @pytest.mark.asyncio
-async def test_fetch_matrix_data_success(monkeypatch):
+async def test_fetch_data_success():
     expected = (
-            '+-----+-----+-----+-----+\n'
-            '|  10 |  20 |  30 |  40 |\n'
-            '+-----+-----+-----+-----+\n'
-            '|  50 |  60 |  70 |  80 |\n'
-            '+-----+-----+-----+-----+\n'
-            '|  90 | 100 | 110 | 120 |\n'
-            '+-----+-----+-----+-----+\n'
-            '| 130 | 140 | 150 | 160 |\n'
-            '+-----+-----+-----+-----+'
+        '+-----+-----+-----+-----+\n'
+        '|  10 |  20 |  30 |  40 |\n'
+        '+-----+-----+-----+-----+\n'
+        '|  50 |  60 |  70 |  80 |\n'
+        '+-----+-----+-----+-----+\n'
+        '|  90 | 100 | 110 | 120 |\n'
+        '+-----+-----+-----+-----+\n'
+        '| 130 | 140 | 150 | 160 |\n'
+        '+-----+-----+-----+-----+'
     )
-    mock_response = AsyncMock()
-    mock_response.text.return_value = expected
-    async def mock_get(url):
-        return mock_response
 
-    monkeypatch.setattr(aiohttp.ClientSession, 'get', mock_get)
-    async with aiohttp.ClientSession() as session:
-        result = await fetch_data('https://example.com', session)
-        assert result == expected
+    with aioresponses() as mocked:
+        mocked.get('https://example.com', status=200, body=expected)
+        async with ClientSession() as session:
+            result = await fetch_data('https://example.com', session)
+            assert result == expected
 
 
 @pytest.mark.asyncio
-async def test_fetch_dataserver_error(monkeypatch):
-    mock_response = AsyncMock()
-    url = URL('https://example.com')
-    headers = CIMultiDict({'Host': 'example.com'})
-    request_info = RequestInfo(
-        url=url,
-        method='GET',
-        headers=headers
-    )
-    mock_response.text.side_effect = ClientResponseError(
-        request_info=request_info, history=(), status=500
-    )
-
-    async def mock_get(url):
-        return mock_response
-
-    monkeypatch.setattr(aiohttp.ClientSession, 'get', mock_get)
-    async with aiohttp.ClientSession() as session:
-        result = await fetch_data('https://example.com', session)
-        assert result == ''
+async def test_fetch_dataserver_error():
+    with aioresponses() as mocked:
+        mocked.get('https://example.com', status=500)
+        async with ClientSession() as session:
+            result = await fetch_data('https://example.com', session)
+            assert result == ''
 
 
 @pytest.mark.asyncio
-async def test_fetch_data_timeout(monkeypatch):
-    # таймаут соединения
-    async def mock_get(url):
-        raise asyncio.TimeoutError
-
-    monkeypatch.setattr(aiohttp.ClientSession, 'get', mock_get)
-
-    async with aiohttp.ClientSession() as session:
-        with pytest.raises(asyncio.TimeoutError):
-            await fetch_data('https://example.com', session)
+async def test_fetch_data_timeout():
+    with aioresponses() as mocked:
+        mocked.get('https://example.com', exception=asyncio.TimeoutError())
+        async with ClientSession() as session:
+            with pytest.raises(asyncio.TimeoutError):
+                await fetch_data('https://example.com', session)
 
 
 @pytest.mark.asyncio
-async def test_fetch_data_connection_refused(monkeypatch):
-    # ошибка соединения
-    async def mock_get(url):
-        os_error = OSError(111, 'Connection refused')
-        raise ClientOSError(os_error.errno, os_error.strerror)
-
-    monkeypatch.setattr(aiohttp.ClientSession, 'get', mock_get)
-
-    async with aiohttp.ClientSession() as session:
-        with pytest.raises(ClientConnectorError):
-            await fetch_data('https://example.com', session)
+async def test_fetch_data_connection_refused():
+    with aioresponses() as mocked:
+        mocked.get('https://example.com', exception=ClientOSError(111, 'Connection refused'))
+        async with ClientSession() as session:
+            with pytest.raises(ClientConnectorError):
+                await fetch_data('https://example.com', session)
