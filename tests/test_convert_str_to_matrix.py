c8dbdc066ef34ec609588ef287b3ca8b0ff21b84
Asgef <asgefes1@gmail.com>
2024-10-28 17:01:25 +0300

    Review test cases

--- tests/test_convert_str_to_matrix.py
+++ tests/test_convert_str_to_matrix.py
@@ -1,10 +1,13 @@
+import pytest
+
+from trainee_assignment.exceptions import MatrixFormatError
 from .test_fetch_data import read_fixture_file
-from trainee_assignment.matrix_utils import convert_str_to_matrix
+from trainee_assignment.matrix_utils import prepare_matrix
 
 
-def test_convert_str_to_matrix_success():
+def test_convert_str_to_matrix_success_1():
     str_data = read_fixture_file('required_matrix.txt')
-    data = convert_str_to_matrix(str_data)
+    data = prepare_matrix(str_data)
     assert data == [
         [10, 20, 30, 40],
         [50, 60, 70, 80],
@@ -12,17 +15,36 @@ def test_convert_str_to_matrix_success():
         [130, 140, 150, 160]
     ]
 
+def test_convert_str_to_matrix_success_2():
+    str_data = (
+        '+---+\n'
+        '|404|\n'
+        '+---+\n'
+    )
+    data = prepare_matrix(str_data)
+    assert data == [
+        [404]
+    ]
+
 
 def test_convert_str_to_matrix_unsuccessful_1():
-    str_data = (
+    with pytest.raises(MatrixFormatError):
+        prepare_matrix(
         '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8">'
         '<title>Title</title></head><body></body></html>'
     )
-    data = convert_str_to_matrix(str_data)
-    assert data == []
 
 
 def test_convert_str_to_matrix_unsuccessful_2():
-    str_data = ''
-    data = convert_str_to_matrix(str_data)
-    assert data == []
+    with pytest.raises(MatrixFormatError):
+        prepare_matrix('')
+
+
+def test_convert_str_to_matrix_unsuccessful_3():
+    with pytest.raises(MatrixFormatError):
+        prepare_matrix('I404I\n')
+
+
+def test_convert_str_to_matrix_unsuccessful_4():
+    with pytest.raises(MatrixFormatError):
+        prepare_matrix('<404>\n')
--- tests/test_matrix_to_spiral.py
+++ tests/test_matrix_to_spiral.py
@@ -18,25 +18,11 @@ data_case_3 = [
         [3, 4]
     ]
 data_case_4 = [[1]]
-data_case_5 = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-        [10, 11, 12]
-    ]
-data_case_6 = [
-        [1, 2, 3],
-        [4, 5, 6]
-    ]
-data_case_7 = []
 
 expected_case_1 = [1, 5, 9, 13, 14, 15, 16, 12, 8, 4, 3, 2, 6, 10, 11, 7]
 expected_case_2 = [1, 4, 7, 8, 9, 6, 3, 2, 5]
 expected_case_3 = [1, 3, 4, 2]
 expected_case_4 = [1]
-expected_case_5 = []
-expected_case_6 = []
-expected_case_7 = []
 
 
 test_cases = [
@@ -44,9 +30,6 @@ test_cases = [
     (matrix_to_spiral, data_case_2, expected_case_2),
     (matrix_to_spiral, data_case_3, expected_case_3),
     (matrix_to_spiral, data_case_4, expected_case_4),
-    (matrix_to_spiral, data_case_5, expected_case_5),
-    (matrix_to_spiral, data_case_6, expected_case_6),
-    (matrix_to_spiral, data_case_7, expected_case_7)
 ]
 
 
